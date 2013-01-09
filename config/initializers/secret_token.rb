9ea42e40167b30c7246a8631f37d32b5e3470d11
Nicole Lewis <lewis.nicole@gmail.com>
2013-01-08 18:53:27 -0800

    thingsibought rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThingsIBought::Application.config.secret_token = '5b0bbd1d25866d504900d11a21a11fd4b1120ab41e0da45feb4a4e802eabb1579484024ca2654b105c4cac5ded7c89dad1431ef80b100189b01ea76e6a3686d4'
